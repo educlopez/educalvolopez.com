f8f2b15a51a47a8bc49c926b8b0d9b5201dd3ddd
educlopez <educlopez93@gmail.com>
2021-03-21 17:22:12 +0100

    Blog tutorial

---
+++ components/date.js
@@ -0,0 +1,7 @@
+import { parseISO, format } from 'date-fns'
+import { es } from 'date-fns/locale';
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy',{ locale: es })}</time>
+}
---
+++ pages/404.js
@@ -0,0 +1,4 @@
+// pages/404.js
+export default function Custom404() {
+  return <h1>404 - La página que busca no se ha encontrado</h1>
+}
