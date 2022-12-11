982c80b92f1e872502a36baef515d20b608ac530
Edu Calvo <educlopez93@gmail.com>
2022-12-11 20:20:02 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL,
+    changefreq: 'daily',
+    priority: 0.7,
     generateRobotsTxt: true,
+    exclude: ['/sinpublicar'],
     sitemapSize: 7000,
   }
