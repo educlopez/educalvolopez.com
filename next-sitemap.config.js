a866109fcdf181c9522e7f61f4fa03e8f52f35cb
Edu Calvo <educlopez93@gmail.com>
2023-01-02 21:36:13 +0100

    fix: change sitemap freq

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL,
-    changefreq: 'weekly',
+    changefreq: 'daily',
     priority: 0.7,
     generateRobotsTxt: true,
     exclude: ['/sinpublicar/*'],
