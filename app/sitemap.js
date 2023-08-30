d6d7427ad2d974021027b79b0308fc920a9d5c99
Edu Calvo <educlopez93@gmail.com>
2023-08-30 16:40:33 +0200

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,9 +1,13 @@
-import { allPosts } from 'contentlayer/generated'
+import { allPosts, allProjects } from 'contentlayer/generated'
 
 export default async function sitemap() {
   const blogs = allPosts.map((post) => ({
-    url: `https://educalvolopez.com/blog/${post.slug}`,
+    url: `https://educalvolopez.com${post.slug}`,
     lastModified: post.publishedAt,
+  }))
+  const projects = allProjects.map((project) => ({
+    url: `https://educalvolopez.com${project.slug}`,
+    lastModified: project.publishedAt,
   }))
 
   const routes = ['', '/about', '/blog', '/stack', '/proyectos'].map(
@@ -13,5 +17,5 @@ export default async function sitemap() {
     })
   )
 
-  return [...routes, ...blogs]
+  return [...routes, ...blogs, ...projects]
 }
